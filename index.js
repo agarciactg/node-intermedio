ff94ae9e0cce5b11484d767716c37f0ba2a1d597
Andres Garcia <agarcia@lsv-tech.com>
2023-01-31 17:21:14 -0500

    Curso Intermedio NODE.

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva_ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 1000,
+    },
+    {
+      name: 'Product 3',
+      price: 1000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Products 1',
+    price: 200,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
