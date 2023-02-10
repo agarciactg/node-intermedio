46d35a385e88749d5d171c09aefc1400736c2c87
Andres Garcia <agarcia@lsv-tech.com>
2023-02-09 22:54:53 -0500

    NODE 🏋🏽‍♂️ - ORM producto creado

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,11 @@
 const { User, UserSchema } = require('./user_model');
 const { Category, CategorySchema } = require('./categories_model');
+const { Product, ProductSchema } = require('./products_models');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
 }
 
 module.exports = setupModels;
--- services/product_services.js
+++ services/product_services.js
@@ -3,6 +3,8 @@ const boom = require('@hapi/boom');
 
 const sequelize = require('../libs/sequelize');
 
+const { models } = require('./../libs/sequelize');
+
 class ProductsService {
   constructor() {
     this.products = [];
@@ -32,13 +34,19 @@ class ProductsService {
   }
 
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const [data, metadata] = await sequelize.query(query);
-    return {
-      data,
-      metadata,
-    };
+    const query = await models.Product.findAll();
+    return query;
   }
+
+  // query sin ORM
+  // async find() {
+  //   const query = 'SELECT * FROM tasks';
+  //   const [data, metadata] = await sequelize.query(query);
+  //   return {
+  //     data,
+  //     metadata,
+  //   };
+  // }
 
   async findOne(id) {
     const product = this.products.find((item) => item.id === id);
